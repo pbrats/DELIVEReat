b0b9699abae68aa21ac5ccc342fd9ef5322c55c3
pbrats <p.bratsioti@gmail.com>
2024-02-08 08:05:24 +0200

    profile component edited to show info/photos signout from profile and routerlink to welcome page

--- src/app/component/profile/profile.component.ts
+++ src/app/component/profile/profile.component.ts
@@ -1,26 +1,35 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
-  authenticatedUser:any;
- ngOnInit(){
-  // Retrieve the stored user information from local storage
-  const storedUser = localStorage.getItem('authenticatedUser');
-  console.log( storedUser);
-  if (storedUser) {
-    // Parse the stored JSON string back into a JavaScript object
-    this.authenticatedUser = JSON.parse(storedUser);
-    console.log(this.authenticatedUser);
-    // Now, this.authenticatedUser contains the information of the authenticated user
-  } else {
-    // Handle the case when no user information is stored in local storage
-    console.log('No user information found in local storage');
+  User:any;
+
+  constructor(private titleService: Title) {
+    titleService.setTitle("Profile");
   }
- }
+  ngOnInit(){ 
+    const storedUser =localStorage.getItem('User');
+    if (storedUser) {
+      // Parse the stored JSON string back into an object
+      this.User = JSON.parse(storedUser);
+      // console.log(this.User);
+      // Now, this.User contains the information of the authenticated user
+    } else {
+      // Handle the case when no user information is stored in local storage
+      console.log('No user information found in local storage');
+    }
+  }
+  SignOut(){
+    sessionStorage.clear();
+    localStorage.clear();
+  }
 }
+
