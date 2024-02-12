e501f27bfca9598c182331b5757a1c00a1d19720
pbrats <p.bratsioti@gmail.com>
2024-02-12 19:41:12 +0200

    provideAnimationsAsync

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,15 +2,15 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { HttpClientModule } from '@angular/common/http';
-// import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
-import { provideAnimations } from '@angular/platform-browser/animations';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+// import { provideAnimations } from '@angular/platform-browser/animations';
 
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     importProvidersFrom(HttpClientModule),
-    // provideAnimationsAsync(),
-    provideAnimations()
+    provideAnimationsAsync()
+    // provideAnimations()
   ]
 };
